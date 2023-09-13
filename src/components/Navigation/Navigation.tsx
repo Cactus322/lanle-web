03f718aed0b6fb8dda83c08c2a0bea67d1ee2a8a
plalgiush <ilya.sushko@shellpea.com>
2023-09-13 18:12:20 +0300

    init commit with login/logout functionality

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,75 +5,96 @@ import {
 	Menu,
 	MenuItem,
 	Link as MUILink,
-} from '@mui/material'
-import PersonIcon from '@mui/icons-material/Person'
-import { useState } from 'react'
-import { switchToRegistration } from '@/reducers/registrationReduser'
-import { connect } from 'react-redux'
-import { switchToRegistrationType } from '@/pages/login/Login.types'
-import Link from 'next/link'
-
-const Navigation = ({
+  } from "@mui/material";
+  import PersonIcon from "@mui/icons-material/Person";
+  import { useState } from "react";
+  import { switchToRegistration } from "@/reducers/registrationReduser";
+  import { connect } from "react-redux";
+  import { switchToRegistrationType } from "@/pages/login/Login.types";
+  import Link from "next/link";
+  import { ILoginSlice } from "@/reducers/loginReducer.types";
+  
+  const Navigation = ({
+	login,
 	switchToRegistration,
-}: {
-	switchToRegistration: switchToRegistrationType
-}) => {
-	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-	const open = Boolean(anchorEl)
+  }: {
+	login: ILoginSlice;
+	switchToRegistration: switchToRegistrationType;
+  }) => {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
 	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-		setAnchorEl(event.currentTarget)
-	}
+	  setAnchorEl(event.currentTarget);
+	};
 	const handleClose = () => {
-		setAnchorEl(null)
-	}
-
+	  setAnchorEl(null);
+	};
+  
+	console.log(login)
+  
 	return (
-		<AppBar position="static">
-			<Toolbar variant="dense">
-				<MUILink
-					variant="h6"
-					component={Link}
-					href="/"
-					sx={{ 
-						flexGrow: 1,
-						color: 'black'
-					}}
-				>
-					Lanle WEB
-				</MUILink>
-				<Button
-					color="inherit"
-					id="basic-button"
-					aria-controls={open ? 'basic-menu' : undefined}
-					aria-haspopup="true"
-					aria-expanded={open ? 'true' : undefined}
-					onClick={handleClick}
-				>
-					<PersonIcon />
-				</Button>
-				<Menu
-					id="basic-menu"
-					anchorEl={anchorEl}
-					open={open}
-					onClose={handleClose}
-					MenuListProps={{
-						'aria-labelledby': 'basic-button',
-					}}
-				>
-					<MenuItem onClick={() => switchToRegistration(false)}>
-						<Link href="/login">Login</Link>
-					</MenuItem>
-					<MenuItem onClick={() => switchToRegistration(true)}>
-						<Link href="/login">Sign In</Link>
-					</MenuItem>
-				</Menu>
-			</Toolbar>
-		</AppBar>
-	)
-}
-
-const mapDispatchProps = {
+	  <AppBar position="static">
+		<Toolbar variant="dense">
+		  <MUILink
+			variant="h6"
+			component={Link}
+			href="/"
+			sx={{
+			  flexGrow: 1,
+			  color: "black",
+			}}
+		  >
+			Lanle WEB
+		  </MUILink>
+		  <Button
+			color="inherit"
+			id="basic-button"
+			aria-controls={open ? "basic-menu" : undefined}
+			aria-haspopup="true"
+			aria-expanded={open ? "true" : undefined}
+			onClick={handleClick}
+		  >
+			<PersonIcon />
+		  </Button>
+		  <Menu
+			id="basic-menu"
+			anchorEl={anchorEl}
+			open={open}
+			onClose={handleClose}
+			MenuListProps={{
+			  "aria-labelledby": "basic-button",
+			}}
+		  >
+			{login.state?.username ? (
+			  <>
+				<MenuItem>{login.state?.username}</MenuItem>
+				<MenuItem onClick={() => window.localStorage.removeItem('loggedUser')}>Logout</MenuItem>
+			  </>
+			) : (
+			  <>
+				<MenuItem onClick={() => switchToRegistration(false)}>
+				  <Link href="/login">Login</Link>
+				</MenuItem>
+				<MenuItem onClick={() => switchToRegistration(true)}>
+				  <Link href="/login">Sign In</Link>
+				</MenuItem>
+			  </>
+			)}
+		  </Menu>
+		</Toolbar>
+	  </AppBar>
+	);
+  };
+  
+  const mapStateToProps = ({ login }: { login: ILoginSlice }) => {
+	return {
+	  login: login,
+	};
+  };
+  
+  const mapDispatchProps = {
 	switchToRegistration,
-}
-
-export default connect(null, mapDispatchProps)(Navigation)
+  };
+  
+  export default connect(mapStateToProps, mapDispatchProps)(Navigation);
+  
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,109 +1,136 @@
-import { useEffect, useState } from 'react'
-import { connect } from 'react-redux'
-import { addUser } from '@/reducers/loginReducer'
-import { setNotice } from '@/reducers/noticeReducer'
-import registrationService from '@/services/registration'
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { addUser } from "@/reducers/loginReducer";
+import { setNotice } from "@/reducers/noticeReducer";
+import registrationService from "@/services/registration";
 
-import { addUserFunctionType } from './Login.types'
-import { setNoticeFunctionType } from '@/types'
+import { addUserFunctionType } from "./Login.types";
+import { setNoticeFunctionType } from "@/types";
 
 import {
-	Box,
-	Button,
-	FormControl,
-	InputLabel,
-	OutlinedInput,
-} from '@mui/material'
-import { ILoginSlice } from '@/reducers/loginReducer.types'
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import { ILoginSlice } from "@/reducers/loginReducer.types";
+import loginService from "@/services/login";
 
 const Login = ({
-	addUser,
-	setNotice,
-	registration,
-	login,
+  addUser,
+  setNotice,
+  registration,
+  login,
 }: {
-	addUser: addUserFunctionType
-	setNotice: setNoticeFunctionType
-	registration: boolean
-	login: ILoginSlice
+  addUser: addUserFunctionType;
+  setNotice: setNoticeFunctionType;
+  registration: boolean;
+  login: ILoginSlice;
 }) => {
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
-	useEffect(() => {
-		if (login.type === 'error') {
-			setNotice('Wrong credentials', 3, 'error')
-		}
-	}, [login])
+  useEffect(() => {
+    if (login.type === "error") {
+      setNotice("Wrong credentials", 3, "error");
+    }
+  }, [login]);
 
-	const handleLogin = (e: React.FormEvent) => {
-		e.preventDefault()
+  console.log(login)
 
-		addUser({ username, password })
-	}
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
 
-	const handleRegistration = async (e: React.FormEvent) => {
-		e.preventDefault
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-		try {
-			await registrationService.registration({ username, password })
-			addUser({ username, password })
-		} catch (exception) {
-			console.error('Wrong credentials')
-		}
-	}
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      // await loginService.login({ username, password })
+      window.localStorage.setItem("loggedUser", JSON.stringify(user));
+      addUser({ username, password });
+      setUser(user.username);
+      setUsername("");
+      setPassword("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-	return (
-		//Интересная ошибка. Если button type = submit, то при попытке зарегистрировать пользователя,
-		//получаем ошибку RequsetAborted
-		<Box component="form" className="position center" autoComplete="true">
-			<FormControl>
-				<InputLabel htmlFor="login-username">Username</InputLabel>
-				<OutlinedInput
-					id="login-username"
-					type="text"
-					label="Username"
-					value={username}
-					onChange={({ target }) => setUsername(target.value)}
-				/>
-			</FormControl>
-			<FormControl>
-				<InputLabel htmlFor="login-username">Password</InputLabel>
-				<OutlinedInput
-					id="login-password"
-					type="text"
-					label="Password"
-					value={password}
-					onChange={({ target }) => setPassword(target.value)}
-				/>
-			</FormControl>
-			<Button
-				variant="outlined"
-				type="button"
-				onClick={registration ? handleRegistration : handleLogin}
-			>
-				{registration ? 'Registration' : 'Login'}
-			</Button>
-		</Box>
-	)
-}
+  const handleRegistration = async (e: React.FormEvent) => {
+    e.preventDefault;
+
+    try {
+      await registrationService.registration({ username, password });
+      addUser({ username, password });
+    } catch (exception) {
+      console.error("Wrong credentials");
+    }
+  };
+
+  // console.log(registration, login.state?.username)
+
+  return (
+    //Интересная ошибка. Если button type = submit, то при попытке зарегистрировать пользователя,
+    //получаем ошибку RequsetAborted
+    <Box component="form" className="position center" autoComplete="true">
+      <FormControl>
+        <InputLabel htmlFor="login-username">Username</InputLabel>
+        <OutlinedInput
+          id="login-username"
+          type="text"
+          label="Username"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="login-username">Password</InputLabel>
+        <OutlinedInput
+          id="login-password"
+          type="text"
+          label="Password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </FormControl>
+      <Button
+        variant="outlined"
+        type="button"
+        onClick={registration ? handleRegistration : handleLogin}
+      >
+        {registration ? "Registration" : "Login"}
+      </Button>
+    </Box>
+  );
+};
 
 const mapStateToProps = ({
-	registration,
-	login,
+  registration,
+  login,
 }: {
-	registration: boolean
-	login: ILoginSlice
+  registration: boolean;
+  login: ILoginSlice;
 }) => {
-	return {
-		registration: registration,
-		login: login,
-	}
-}
+  return {
+    registration: registration,
+    login: login,
+  };
+};
 
 const mapDispatchProps = {
-	addUser,
-	setNotice,
-}
+  addUser,
+  setNotice,
+};
 
-export default connect(mapStateToProps, mapDispatchProps)(Login)
+export default connect(mapStateToProps, mapDispatchProps)(Login);
